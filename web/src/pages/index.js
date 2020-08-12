bb2b38a24cd731dc870a4453a175501dee22631a
Zach Vasquez <zachopelaco@gmail.com>
2020-08-12 18:14:39 -0500

    Update index.js

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import { mapEdgesToNodes, filterOutDocsWithoutSlugs } from '../lib/helpers'
-import BlogPostPreviewGrid from '../components/blog-post-preview-grid'
-import Container from '../components/container'
+
 import GraphQLErrorList from '../components/graphql-error-list'
-import ProjectPreviewGrid from '../components/project-preview-grid'
 import SEO from '../components/seo'
 import Layout from '../containers/layout'
+
+import Layout1 from '../components/layout1'
+
+import { Link } from "gatsby"
+import { getUser, isLoggedIn } from "../services/auth"
+
+import BlogPostPreviewGrid from '../components/blog-post-preview-grid'
+import Container from '../components/container'
+import ProjectPreviewGrid from '../components/project-preview-grid'
 
 export const query = graphql`
   query IndexPageQuery {
@@ -91,50 +98,63 @@ export const query = graphql`
 
 const IndexPage = props => {
   const { data, errors } = props
-
-  if (errors) {
-    return (
-      <Layout>
-        <GraphQLErrorList errors={errors} />
-      </Layout>
-    )
+    
+    if (errors) {
+        return (
+            <Layout>
+                <GraphQLErrorList errors={errors} />
+            </Layout>
+        )
   }
+    const site = (data || {}).site
+    const postNodes = (data || {}).posts
+        ? mapEdgesToNodes(data.posts).filter(filterOutDocsWithoutSlugs)
+        : []
+    const projectNodes = (data || {}).projects
+        ? mapEdgesToNodes(data.projects).filter(filterOutDocsWithoutSlugs)
+        : []
 
-  const site = (data || {}).site
-  const postNodes = (data || {}).posts
-    ? mapEdgesToNodes(data.posts).filter(filterOutDocsWithoutSlugs)
-    : []
-  const projectNodes = (data || {}).projects
-    ? mapEdgesToNodes(data.projects).filter(filterOutDocsWithoutSlugs)
-    : []
-
-  if (!site) {
-    throw new Error(
-      'Missing "Site settings". Open the studio at http://localhost:3333 and add some content to "Site settings" and restart the development server.'
-    )
-  }
-
+    if (!site) {
+        throw new Error(
+        'Missing "Site settings". Open the studio at http://localhost:3333 and add some content to "Site settings" and restart the development server.'
+        )
+    }
+  
   return (
-    <Layout>
-      <SEO title={site.title} description={site.description} keywords={site.keywords} />
-      <Container>
-        <h1 hidden>Welcome to {site.title}</h1>
-        {projectNodes && (
-          <ProjectPreviewGrid
-            title='Latest projects'
-            nodes={projectNodes}
-            browseMoreHref='/projects/'
-          />
-        )}
-        {postNodes && (
-          <BlogPostPreviewGrid
-            title='Latest blog posts'
-            nodes={postNodes}
-            browseMoreHref='/blog/'
-          />
-        )}
-      </Container>
-    </Layout>
+    <Layout1>
+    <h1>Hello {isLoggedIn() ? getUser().name : "world"}!</h1>
+    <p>
+      {isLoggedIn() ? (
+        <>
+          <Layout>
+          <SEO title={site.title} description={site.description} keywords={site.keywords} />
+            <Container>
+                <h1 hidden>Welcome to {site.title}</h1>
+                {projectNodes && (
+                    <ProjectPreviewGrid
+                    title='Latest projects'
+                    nodes={projectNodes}
+                    browseMoreHref='/projects/'
+                    />
+                )}
+                {postNodes && (
+                    <BlogPostPreviewGrid
+                    title='Latest blog posts'
+                    nodes={postNodes}
+                    browseMoreHref='/blog/'
+                    />
+                )}
+            </Container>
+        </Layout>
+        </>
+      ) : (
+        <>
+          You should <Link to="/app/login">log in</Link> to see restricted
+          content
+        </>
+      )}
+    </p>
+  </Layout1>
   )
 }
 
