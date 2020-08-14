f99e3d49a83656d9e9fd9bb63e6ab356833c53c9
Zach Vasquez <zachopelaco@gmail.com>
2020-08-14 16:20:51 -0500

    Create serviceData.js

---
+++ studio/schemas/serviceData.js
@@ -0,0 +1,40 @@
+export default {
+    title: 'Service Data',
+    type: 'object',
+    name: 'serviceData',
+    fields: [
+        {
+            title: 'Service Type',
+            name: 'serviceType',
+            type: 'string',
+            options: {
+                list: [
+                    'Free In-Home Estimate',
+                    'Water Testing',
+                    'Warranty and Service',
+                    'Salt Delivery',
+                    'Existing System Service',
+                    'Softener Rentals'
+                ]
+
+            },
+        },
+        {
+            
+            title: 'Date',
+            name: 'date',
+            type: 'date'
+        },
+        {
+            title: 'Description/Service Notes',
+            name: 'description',
+            type: 'text'
+        },
+        {
+            title: 'Cost of visit',
+            name: 'cost',
+            type: 'number'
+        },
+
+    ]
+}
