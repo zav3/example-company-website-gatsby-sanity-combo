f2d8d0ce54b742f895b04668495c166a9a11686d
Zach Vasquez <zachopelaco@gmail.com>
2020-08-14 16:20:40 -0500

    Create saltDelivery.js

---
+++ studio/schemas/saltDelivery.js
@@ -0,0 +1,12 @@
+export default {
+    title: 'Salt Delivery',
+    name: 'saltDelivery',
+    type: 'object',
+    fields: [
+        {
+            title: 'Number of bags',
+            name: 'numOfBags',
+            type: 'number'
+        }
+    ]
+}
