0e7a27128c290f4f48049b7e780dc00f33d27394
Zach Vasquez <zachopelaco@gmail.com>
2020-08-19 17:06:24 -0500

    Create clients.js

---
+++ web/src/pages/clients.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import Container from '../components/container'
+import GraphQLErrorList from '../components/graphql-error-list'
+import SEO from '../components/seo'
+import Layout from '../containers/layout'
+import { mapEdgesToNodes, filterOutDocsWithoutSlugs } from '../lib/helpers'
+import { responsiveTitle1 } from '../components/typography.module.css'
+
+import ClientPreview from '../components/client-preview'
+
+export const query = graphql`
+  query clientPageQuery {
+    clients: allSanityClient {
+      edges {
+        node {
+          id
+          name
+        }
+      }
+    }
+  }
+`
+
+const ClientsPage = props => {
+    const { data, errors } = props
+    if (errors) {
+    return (
+      <Layout>
+        <GraphQLErrorList errors={errors} />
+      </Layout>
+    )
+    }
+    const clientNodes =
+    data && data.clients && mapEdgesToNodes(data.clients)
+    return (
+    <Layout>
+      <SEO title='Clients' />
+      <Container>
+        <h1 className={responsiveTitle1}>Clients</h1>
+        {clientNodes && clientNodes.length > 0 && <ClientPreview nodes={clientNodes} />}
+      </Container>
+    </Layout>
+  )
+}
+
+export default ClientsPage
