805e1d19dd5bcf10bd4e8e7cd70dc32311b4bd02
Zach Vasquez <zachopelaco@gmail.com>
2020-08-27 17:05:14 -0500

    Update saltDelivery.js

--- studio/schemas/saltDelivery.js
+++ studio/schemas/saltDelivery.js
@@ -4,9 +4,29 @@ export default {
     type: 'object',
     fields: [
         {
+            title: 'Auto Salt',
+            name: 'autoSalt',
+            type: 'boolean'
+        },
+        {
             title: 'Number of bags',
             name: 'numOfBags',
             type: 'number'
-        }
+        },
+        {
+            title: 'Last Salt',
+            name: 'lastSalt',
+            type: 'date'
+        },
+        {
+            title: 'Next Salt',
+            name: 'nextSalt',
+            type: 'date'
+        },
+        {
+            title: 'Notes for Salt Delivery',
+            name: 'saltDescription',
+            type: 'text'
+        },
     ]
 }
