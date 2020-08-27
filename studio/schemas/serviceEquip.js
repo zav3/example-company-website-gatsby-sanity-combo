c17b576a55130b754c950695c2bd71e4ae0ae31e
Zach Vasquez <zachopelaco@gmail.com>
2020-08-27 17:05:20 -0500

    Create serviceEquip.js

---
+++ studio/schemas/serviceEquip.js
@@ -0,0 +1,17 @@
+export default {
+    name: 'serviceEquip',
+    title: 'Service Equipment',
+    type: 'object',
+    fields: [
+        {
+            name: 'equipmentUsed',
+            title: 'Equipment Used',
+            type: 'string'
+        },
+        {
+            name: 'serialNum',
+            title: 'Serial #',
+            type: 'string'
+        },
+    ]
+}
