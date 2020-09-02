4986a43758a3df9e0dc7d7c50d44b3ccbc6cee50
Zach Vasquez <zachopelaco@gmail.com>
2020-09-02 12:54:41 -0500

    Update client.js

--- web/src/components/client.js
+++ web/src/components/client.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import Container from './container'
 import styles from './project-preview-grid.module.css'
+import { Link } from 'gatsby'
+import { cn } from '../lib/helpers'
+import { responsiveTitle3 } from './typography.module.css'
+import ServicePreview from './servicePreview'
+
 
 function Client (props) {
   const { lastName, firstName, phoneNumber1, phoneNumber2, email, address, subdivision, city, zipcode, services } = props
@@ -17,15 +22,65 @@ function Client (props) {
                 </ul>
               </div>
             )}
+            {phoneNumber2 && (
+              <div className={styles.relatedProjects}>
+                <h3 className={styles.relatedProjectsHeadline}>Phone #2</h3>
+                <ul>
+                  {phoneNumber2}
+                </ul>
+              </div>
+            )}
+            {email && (
+              <div className={styles.relatedProjects}>
+                <h3 className={styles.relatedProjectsHeadline}>Email</h3>
+                <ul>
+                  {email}
+                </ul>
+              </div>
+            )}
+            {address && (
+              <div className={styles.relatedProjects}>
+                <h3 className={styles.relatedProjectsHeadline}>Address</h3>
+                <ul>
+                  {address}
+                </ul>
+              </div>
+            )}
+            {subdivision && (
+              <div className={styles.relatedProjects}>
+                <h3 className={styles.relatedProjectsHeadline}>Subdivision</h3>
+                <ul>
+                  {subdivision}
+                </ul>
+              </div>
+            )}
+            {city && (
+              <div className={styles.relatedProjects}>
+                <h3 className={styles.relatedProjectsHeadline}>City</h3>
+                <ul>
+                  {city}
+                </ul>
+              </div>
+            )}
+            {zipcode && (
+              <div className={styles.relatedProjects}>
+                <h3 className={styles.relatedProjectsHeadline}>Zipcode</h3>
+                <ul>
+                  {zipcode}
+                </ul>
+              </div>
+            )}
         </div>
         <aside className={styles.metaContent}>
             {services && (
               <div className={styles.categories}>
                 <h3 className={styles.categoriesHeadline}>Services</h3>
                 <ul>
-                  {services.map(service => (
-                    <li key={service._id}>{service.serviceType}</li>
-                  ))}
+                  {services.map(node => (
+                      <li key={node.id}>
+                        <ServicePreview {...node} />
+                      </li>
+                    ))}
                 </ul>
               </div>
             )}
