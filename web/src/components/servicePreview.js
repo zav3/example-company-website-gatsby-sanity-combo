176ce4502fa25cecf8e775ec11f8bca2f84b17a0
Zach Vasquez <zachopelaco@gmail.com>
2020-09-02 12:54:47 -0500

    Create servicePreview.js

---
+++ web/src/components/servicePreview.js
@@ -0,0 +1,16 @@
+import { Link } from 'gatsby'
+import React from 'react'
+import { cn } from '../lib/helpers'
+
+import styles from './project-preview.module.css'
+import { responsiveTitle3 } from './typography.module.css'
+
+function ServicePreview (props) {
+  return (
+    <Link className={styles.root} to={`/servicePage/${props}`}>
+        <h3 className={cn(responsiveTitle3, styles.title)}>{props.serviceType}</h3>
+    </Link>
+  )
+}
+
+export default ServicePreview
