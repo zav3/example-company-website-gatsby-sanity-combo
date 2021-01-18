829dc54e3fa8d4c16a951e4a1bb2e6a275267899
Zach Vasquez <zachopelaco@gmail.com>
2021-01-18 14:47:14 -0600

    Update service.js

--- web/src/templates/service.js
+++ web/src/templates/service.js
@@ -5,47 +5,32 @@ import GraphQLErrorList from '../components/graphql-error-list'
 import SEO from '../components/seo'
 import Layout from '../containers/layout'
 import Service from '../components/service'
+const componentsRenderer = components => {
 
-export const query = graphql`
-  query ServiceTemplateQuery($id: String!) {
-    serviceData: sanityClient(id: { eq: $id }) {
-      services {
-        serviceType
-        serviceDate 
-        salesAmount
-        equipment {
-          equipmentUsed
-        }
-        lastService1 
-        cardSent1 
-        nextServiceDue1 
-        description1 
-        lastService2
-        cardSent2 
-        nextServiceDue2 
-        description2 
-        ac
-        ro 
-        description3
+  return components.map(component => {
+      let children = []
+      if (component.children){
+          children = componentsRenderer(component.children)
       }
-    }
-  }
-`
+      const element = require(`${HOME_DIR}/${component.path}`)
 
+      return React.createElement(element, Object.assign({},{key: component.key},{...component.props}),children)
+  });
+}
 const ServiceTemplate = props => {
-  const { data, errors } = props
-  const serviceData = data && data.serviceData
+  const data = props.pageContext.children
   return (
     <Layout>
-      {errors && <SEO title='GraphQL Error' />}
-      {serviceData && <SEO title={serviceData.serviceType || 'Untitled'} />}
+      {/* {errors && <SEO title='GraphQL Error' />}
+      {serviceData && <SEO title={serviceData.serviceDate || 'Untitled'} />}
 
       {errors && (
         <Container>
           <GraphQLErrorList errors={errors} />
         </Container>
       )}
-      {serviceData && <Service {...serviceData} />}
+      {serviceData && <Service {...serviceData} />} */}
+      {componentsRenderer(data)}
     </Layout>
   )
 }
